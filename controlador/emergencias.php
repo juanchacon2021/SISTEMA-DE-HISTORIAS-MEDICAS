20bb50d58e3a7b0546925eee9d41098367a431b9
anthoan23 <anthoan.g23gmail.com>
2024-07-05 00:19:21 -0400

    emergencias

---
+++ controlador/emergencias.php
@@ -0,0 +1,68 @@
+<?php
+  
+//llamada al archivo que contiene la clase
+//usuarios, en ella estara el codigo que me //permitirá
+//guardar, consultar y modificar dentro de mi base //de datos
+
+
+//lo primero que se debe hacer es verificar al //igual que en la vista que exista el archivo
+if (!is_file("modelo/".$pagina.".php")){
+	//alli pregunte que si no es archivo se niega //con !
+	//si no existe envio mensaje y me salgo
+	echo "Falta definir la clase ".$pagina;
+	exit;
+}  
+require_once("modelo/".$pagina.".php");  
+  if(is_file("vista/".$pagina.".php")){
+	  
+	  //bien si estamos aca es porque existe la //vista y la clase
+	  //por lo que lo primero que debemos hace es //realizar una instancia de la clase
+	  //instanciar es crear una variable local, //que contiene los metodos de la clase
+	  //para poderlos usar
+	  
+	  
+	  
+	  
+	  
+	  if(!empty($_POST)){
+		$o = new emergencias();   
+		  //como ya sabemos si estamos aca es //porque se recibio alguna informacion
+		  //de la vista, por lo que lo primero que //debemos hacer ahora que tenemos una 
+		  //clase es guardar esos valores en ella //con los metodos set
+		  $accion = $_POST['accion'];
+		  
+          if($accion=='mostrar'){
+            echo  json_encode($o->mostrar());  
+         }
+		  if($accion=='consultar'){
+			 echo  json_encode($o->consultar());  
+		  }
+		  elseif($accion=='eliminar'){
+			 $o->set_cedula_h($_POST['cedula_h']);
+			 echo  json_encode($o->eliminar());
+		  }
+		  else{	          	  
+			  $o->set_horaingreso($_POST['horaingeso']);
+			  $o->set_fechaingreso($_POST['fechaingreso']);
+			  $o->set_motingreso($_POST['motingreso']);
+			  $o->set_diagnostico_e($_POST['diagnostico_e']);
+			  $o->set_tratamientos($_POST['tratamientos']);
+			  $o->set_cedula_p($_POST['cedula_p']);
+              $o->set_cedula_h($_POST['cedula_h']);
+			  if($accion=='incluir'){
+				echo  json_encode($o->incluir());
+			  }
+			  elseif($accion=='modificar'){
+				echo  json_encode($o->modificar());
+			  }
+		  }
+		  exit;
+	  }
+	  
+	  
+	  require_once("vista/".$pagina.".php"); 
+  }
+  else{
+	  echo "pagina en construccion";
+  }
+?>
