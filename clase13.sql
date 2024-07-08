b335a206dd852433924b818f61b2f7f84ee8147c
Your Name <chaconjuan439@gmail.com>
2024-07-07 21:22:47 -0400

    si

--- clase13.sql
+++ clase13.sql
@@ -25,9 +25,9 @@ DROP TABLE IF EXISTS `consultas`;
 CREATE TABLE `consultas` (
   `cod_consulta` int NOT NULL AUTO_INCREMENT,
   `fechaconsulta` date DEFAULT NULL,
-  `consulta` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `diagnostico` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `tratamientos` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `consulta` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `diagnostico` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `tratamientos` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `cedula_p` int NOT NULL,
   `cedula_h` int NOT NULL,
   PRIMARY KEY (`cod_consulta`),
@@ -35,7 +35,7 @@ CREATE TABLE `consultas` (
   KEY `cedula_h` (`cedula_h`),
   CONSTRAINT `consultas_ibfk_1` FOREIGN KEY (`cedula_p`) REFERENCES `personal` (`cedula_personal`),
   CONSTRAINT `consultas_ibfk_2` FOREIGN KEY (`cedula_h`) REFERENCES `historias` (`cedula_historia`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,6 +44,7 @@ CREATE TABLE `consultas` (
 
 LOCK TABLES `consultas` WRITE;
 /*!40000 ALTER TABLE `consultas` DISABLE KEYS */;
+INSERT INTO `consultas` VALUES (31,'2024-05-07','ginecologia','ninguno','ninguno',31,30128924);
 /*!40000 ALTER TABLE `consultas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,11 +57,11 @@ DROP TABLE IF EXISTS `emergencias`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `emergencias` (
   `cod_emergencia` int NOT NULL AUTO_INCREMENT,
-  `horaingreso` varchar(7) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `horaingreso` varchar(7) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `fechaingreso` date DEFAULT NULL,
-  `motingreso` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `diagnostico_e` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `tratamientos` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `motingreso` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `diagnostico_e` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `tratamientos` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `cedula_p` int NOT NULL,
   `cedula_h` int NOT NULL,
   PRIMARY KEY (`cod_emergencia`),
@@ -90,13 +91,13 @@ DROP TABLE IF EXISTS `examenes`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `examenes` (
   `cod_examenes` int NOT NULL AUTO_INCREMENT,
-  `tipo_examen` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `observacion_medica` varchar(300) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `tipo_examen` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `observacion_medica` varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `cedula_h` int NOT NULL,
   PRIMARY KEY (`cod_examenes`),
   KEY `cedula_h` (`cedula_h`),
   CONSTRAINT `examenes_ibfk_1` FOREIGN KEY (`cedula_h`) REFERENCES `historias` (`cedula_historia`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,6 +106,7 @@ CREATE TABLE `examenes` (
 
 LOCK TABLES `examenes` WRITE;
 /*!40000 ALTER TABLE `examenes` DISABLE KEYS */;
+INSERT INTO `examenes` VALUES (1,'Sanguineo','Planquetas Altas',30128924);
 /*!40000 ALTER TABLE `examenes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -117,31 +119,31 @@ DROP TABLE IF EXISTS `historias`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `historias` (
   `cedula_historia` int NOT NULL,
-  `nombre` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
-  `apellido` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  `nombre` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `apellido` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `fecha_nac` date DEFAULT NULL,
-  `direccion` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `telefono` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `ocupacion` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `direccion` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `telefono` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `ocupacion` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `edad` int DEFAULT NULL,
-  `estadocivi` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `habtoxico` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `hda` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `alergias` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `quirurgico` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `transsanguineo` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `boca_abierta` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `boca_cerrada` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `oidos` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `cabeza_craneo` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `ojos` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `nariz` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `tiroides` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `cardiovascular` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `respiratorio` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `abdomen` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `extremidades` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `neurologico` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `estadocivi` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `habtoxico` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `hda` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `alergias` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `quirurgico` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `transsanguineo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `boca_abierta` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `boca_cerrada` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `oidos` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `cabeza_craneo` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `ojos` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nariz` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `tiroides` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `cardiovascular` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `respiratorio` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `abdomen` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `extremidades` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `neurologico` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`cedula_historia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -152,7 +154,7 @@ CREATE TABLE `historias` (
 
 LOCK TABLES `historias` WRITE;
 /*!40000 ALTER TABLE `historias` DISABLE KEYS */;
-INSERT INTO `historias` VALUES (31111553,'franchesco','virgoline',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO `historias` VALUES (30128924,'Juan Esteban','Chacon Salazar','2004-04-05','Cerritos Blanco','04121091985','aaaaaaaaaa',20,'DIVORCIADO','aaaaaaa','aaaaaaaa','aaaaaaa','aaaaa','aaaaaaa','aaaaaa','aaaaaaaa','aaaaaa','aaaaaa','aaaaaa','aaaaaa','aaaaa','aaaaa','aaaaaa','aaaaaa','aaaaaa','aaaaaaa'),(31111553,'Anthoan','Gonzalez','2024-07-01','Yucatan','04120754296','bbbbbbbb',32,'SOLTERO','bbbbbbbb','bbbbbbbb','bbbbbbbb','bbbb','bbbbbb','bbbbbb','bbbbbbb','bbbbbb','bbbbbb','bbbbb','bbbb','bbbb','bbbb','bbbb','bbbb','bbbbb','bbbb');
 /*!40000 ALTER TABLE `historias` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -165,11 +167,11 @@ DROP TABLE IF EXISTS `personal`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `personal` (
   `cedula_personal` int NOT NULL,
-  `nombre` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `apellido` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `correo` varchar(30) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `telefono` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `cargo` varchar(15) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nombre` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `apellido` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `correo` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `telefono` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `cargo` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`cedula_personal`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -180,7 +182,7 @@ CREATE TABLE `personal` (
 
 LOCK TABLES `personal` WRITE;
 /*!40000 ALTER TABLE `personal` DISABLE KEYS */;
-INSERT INTO `personal` VALUES (31,'anthoan','gonzalez','nose','3111','doctor');
+INSERT INTO `personal` VALUES (31,'Jorge','Patino','correo@correo.com','04121111111','doctor');
 /*!40000 ALTER TABLE `personal` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -193,4 +195,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-03 14:31:45
+-- Dump completed on 2024-07-05  0:41:11
