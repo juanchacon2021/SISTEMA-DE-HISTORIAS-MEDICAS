9aa050d49e8c8ae42aba3d9a22fbe2f47c9d6577
anthoan23 <anthoan.g23gmail.com>
2024-07-11 20:25:38 -0400

    emergencias

--- vista/emergencias.php
+++ vista/emergencias.php
@@ -8,7 +8,87 @@
 <div class="container texto-bienvenida h2 text-center py-8 text-zinc-800">
 Emergencias
 </div>
-<div class="container pl-64"> <!-- todo el contenido ira dentro de esta etiqueta-->
+<!--nuevo-->
+<div class="container">	
+				<div class="row mb-3">
+				
+				<div class="col-md-4" style="display: none;">
+					   <label for="cod_emergencia">cod_emergencia</label>
+					   <input class="form-control" type="text" id="cod_emergencia" />
+					   <span id="scod_emergencia"></span>
+					</div>
+					
+					<div class="col-md-4">
+					   <label for="horaingreso">Hora de Ingreso</label>
+					   <input class="form-control" type="text" id="horaingreso" />
+					   <span id="shoraingreso"></span>
+					</div>
+
+                    <div class="col-md-4">
+					   <label for="fechaingreso">Fecha de Ingreso</label>
+					   <input class="form-control" type="date" id="fechaingreso" />
+					   <span id="fechaingreso"></span>
+					</div>
+
+					
+				</div>
+				
+				<div class="col-md-4">
+					   <label for="motingreso">Motivo de Ingrso</label>
+					   <input class="form-control" type="text" id="motingreso" />
+					   <span id="smotingreso"></span>
+					</div>
+
+				<div class="row mb-3">
+
+					<div class="col-md-12">
+					   <label for="diagnostico_e">Diagnostico</label>
+					   <input class="form-control" type="text" id="diagnostico_e"  />
+					   <span id="sdiagnostico_e"></span>
+					</div>
+                    
+				</div>
+				<div class="row mb-3">
+
+					<div class="col-md-12">
+					   <label for="tratamientos">Tratamientos</label>
+					   <input class="form-control" type="text" id="tratamientos"/>
+					   <span id="stratamientos"></span>
+					</div>
+                    
+				</div>
+
+
+				<div class="row mb-3">
+
+					<div class="col-md-12">
+					   <label for="cedula_p">Cedula_p</label>
+					   <input class="form-control" type="text" id="cedula_p"  />
+					   <span id="scedula_p"></span>
+					</div>
+					<div class="col-md-12">
+					   <label for="cedula_h">Cedula_h</label>
+					   <input class="form-control" type="text" id="cedula_h"  />
+					   <span id="scedula_h"></span>
+					</div>
+                    
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<hr/>
+					</div>
+				</div>
+
+				<div class="row mt-3 justify-content-center">
+					<div class="col-md-2">
+						   <button type="button" class="btn btn-primary" 
+						   id="proceso" style="background: #FF0000"></button>
+					</div>
+				</div>
+			</div>
+
+<!--nuevo-->
+<div class="container pl-64">
 	<div class="container">
 		<div class="row mt-3 justify-content-between">
 		    <div class="col-md-2 boton" onclick='pone(this,3)' >
@@ -43,10 +123,9 @@ Emergencias
 	   </table>
 	  </div>
   </div>
-</div> <!-- fin de container -->
+</div> 
 
 
-<!-- seccion del modal -->
 <div class="modal fade" tabindex="-1" role="dialog"  id="modal1">
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
@@ -54,7 +133,7 @@ Emergencias
         <h5 class="modal-title">Formulario de Emergencias</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
-		<div class="container"> <!-- todo el contenido ira dentro de esta etiqueta-->
+		<div class="container">
 		   <form method="post" id="f" autocomplete="off">
 			<input autocomplete="off" type="text" class="form-control" name="accion" id="accion" style="display: none;">
 			<div class="container">	
@@ -135,17 +214,13 @@ Emergencias
 				</div>
 			</div>	
 			</form>
-		</div> <!-- fin de container -->
-		<!--
+		</div> 
 		
-		-->
     </div>
 	
   </div>
 </div>
 
-<!--fin de seccion modal-->
-<!--Llamada a archivo modal.php, dentro de el hay una sección modal-->
 <?php require_once("comunes/modal.php"); ?>
 <script type="text/javascript" src="js/emergencias.js"></script> 
 
