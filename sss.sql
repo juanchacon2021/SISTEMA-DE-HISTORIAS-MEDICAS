09e1de2054d6031f3476abc6c9689a4de0b3136f
Your Name <chaconjuan439@gmail.com>
2024-10-20 19:43:10 -0400

    cambio de base de datos

--- sss.sql
+++ sss.sql
@@ -31,7 +31,7 @@ CREATE TABLE `antecedentes` (
   PRIMARY KEY (`cod_antecedente`),
   KEY `cedula_h` (`cedula_h`),
   CONSTRAINT `antecedentes_ibfk_1` FOREIGN KEY (`cedula_h`) REFERENCES `historias` (`cedula_historia`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,7 +63,7 @@ CREATE TABLE `consultas` (
   KEY `cedula_h` (`cedula_h`),
   CONSTRAINT `consultas_ibfk_1` FOREIGN KEY (`cedula_p`) REFERENCES `personal` (`cedula_personal`),
   CONSTRAINT `consultas_ibfk_2` FOREIGN KEY (`cedula_h`) REFERENCES `historias` (`cedula_historia`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,7 +72,6 @@ CREATE TABLE `consultas` (
 
 LOCK TABLES `consultas` WRITE;
 /*!40000 ALTER TABLE `consultas` DISABLE KEYS */;
-INSERT INTO `consultas` VALUES (2,'2024-10-20','hola','hola','hola',12345678,33333333),(3,'2024-10-08','ss','ss','ss',31111553,12122122),(4,'2024-10-18','ss','sss','sss',12345678,33333333),(5,'2024-10-21','rr','rrr','rrrr',31111553,33333333),(6,'2024-10-10','tt','tt','tt',12345678,33333333),(7,'2024-11-02','yy','yyy','yyy',12345678,33333333),(8,'2024-10-26','uu','uuu','uuu',12345678,24222543),(9,'2024-10-11','tt','tt','ttt',31111553,12122122),(10,'2024-10-14','jjjjjjjjjjjjjjjjj','jjjjjjjjjjjjjjjjj','jjjjjjjjjjjjj',31111553,24222543),(11,'2024-10-22','yyyyyyyyyyyyyyyyyy','yyyyyyyyyyyy','yyyyyyyyyy',31111553,33333333),(12,'2024-10-15','llego marico','le guta el pene','dejar de ser marico y una buena totona',12345678,12122122),(13,'2024-10-15','ggg','ggg','ggg',31111553,12122122),(14,'2024-10-15','yyy','yyy','yyyy',31111553,24222543),(15,'2024-10-15','uuu','uuu','uuuu',31111553,33333333),(16,'2024-10-15','uu','uuu','uuu',31111553,12122122);
 /*!40000 ALTER TABLE `consultas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -97,7 +96,7 @@ CREATE TABLE `emergencias` (
   KEY `cedula_h` (`cedula_h`),
   CONSTRAINT `emergencias_ibfk_1` FOREIGN KEY (`cedula_p`) REFERENCES `personal` (`cedula_personal`),
   CONSTRAINT `emergencias_ibfk_2` FOREIGN KEY (`cedula_h`) REFERENCES `historias` (`cedula_historia`)
-) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -106,7 +105,7 @@ CREATE TABLE `emergencias` (
 
 LOCK TABLES `emergencias` WRITE;
 /*!40000 ALTER TABLE `emergencias` DISABLE KEYS */;
-INSERT INTO `emergencias` VALUES (14,'22:40','2024-08-28','chaooo','es pato ','patosida',31111553,33333333),(16,'23:34','2024-08-14','es marico','que es marico','dejar de ser marico',31111553,33333333),(19,'12:36','2024-09-04','eduin lo apoñalo','metale el dedo en el culo','metanle dos dedos en el culo',12345678,33333333),(20,'18:42','2024-10-26','gg','gg','gg',31111553,33333333),(23,'20:57','2024-10-21','','','',12345678,24222543),(25,'06:06','2024-10-30','hhhhh','hhhhh','hhhh',31111553,24222543),(29,'06:11','2024-10-09','ssss','ssss','ssss',31111553,24222543),(30,'09:06','2024-10-23','sss','sss','ss',31111553,24222543),(31,'09:06','2024-10-23','sss','sss','ss',31111553,24222543),(32,'06:20','2024-10-28','qqqq','qqq','qqq',31111553,24222543),(33,'06:19','2024-10-28','aaaa','aaa','aaa',12345678,24222543),(34,'06:26','2024-10-29','rrrrrr','rrrr','rrrr',31111553,24222543),(36,'08:11','2024-10-29','rr','rr','rr',31111553,33333333),(37,'08:11','2024-10-13','ss','ss','ss',12345679,24222543),(41,'22:13','2024-10-09','see','eee','eee',12345678,24222543),(42,'06:40','2024-10-08','ss','ss','ssss',12345678,33333333),(43,'15:47','2024-11-09','porque es marico ','en verdad es marico','dejar de ser marico',31111553,24222543),(44,'11:11','2024-10-07','ddd','fff','fff',12345678,33333333),(46,'05:35','2024-10-06','le duele el cuerpo','es un flojo','penesilina',31111553,12122122),(47,'06:18','2024-10-09','ss','ss','sss',12345678,33333333),(48,'18:29','2024-10-08','tt','ttt','tt',31111553,33333333),(49,'06:29','2024-10-29','rr','rr','rrr',31111553,24222543),(50,'10:14','2024-10-15','tttt','ggg','ggg',31111553,12122122);
+INSERT INTO `emergencias` VALUES (14,'22:40','2024-08-28','chaooo','es pato ','patosida',31111553,33333333),(16,'23:34','2024-08-14','es marico','que es marico','dejar de ser marico',31111553,33333333),(18,'14:30','2024-08-29','tubo un golpe en la cabeza','traumatismo','pastillas para el dolor de cabeza ',31,24222543);
 /*!40000 ALTER TABLE `emergencias` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -120,7 +119,7 @@ DROP TABLE IF EXISTS `examenes`;
 CREATE TABLE `examenes` (
   `cod_examenes` int NOT NULL AUTO_INCREMENT,
   `nombre_examen` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
-  `descripcion_examen` varchar(300) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `observacion_medica` varchar(300) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`cod_examenes`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -142,7 +141,7 @@ DROP TABLE IF EXISTS `examenes_f`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `examenes_f` (
-  `cod_examen_f` int NOT NULL AUTO_INCREMENT,
+  `cod_examen_f` int NOT NULL,
   `general` varchar(300) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `cedula_h` int NOT NULL,
   PRIMARY KEY (`cod_examen_f`),
@@ -168,7 +167,7 @@ DROP TABLE IF EXISTS `examenes_r`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `examenes_r` (
-  `cod_examen_r` int NOT NULL AUTO_INCREMENT,
+  `cod_examen_r` int NOT NULL,
   `cabeza_craneo` varchar(300) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `ojos` varchar(300) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `nariz` varchar(300) COLLATE utf8mb4_general_ci DEFAULT NULL,
@@ -181,7 +180,7 @@ CREATE TABLE `examenes_r` (
   PRIMARY KEY (`cod_examen_r`),
   KEY `cedula_h` (`cedula_h`),
   CONSTRAINT `examenes_r_ibfk_1` FOREIGN KEY (`cedula_h`) REFERENCES `historias` (`cedula_historia`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -257,7 +256,7 @@ CREATE TABLE `historias` (
 
 LOCK TABLES `historias` WRITE;
 /*!40000 ALTER TABLE `historias` DISABLE KEYS */;
-INSERT INTO `historias` VALUES (12122122,'cambur','verde','2012-12-17',23,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(24222543,'mago','pinton','2012-12-17',23,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(33333333,'jose','jose','2012-12-17',23,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+INSERT INTO `historias` VALUES (24222543,'mago','pinton','2012-12-17',23,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(33333333,'jose','jose','2012-12-17',23,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `historias` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -285,7 +284,7 @@ CREATE TABLE `personal` (
 
 LOCK TABLES `personal` WRITE;
 /*!40000 ALTER TABLE `personal` DISABLE KEYS */;
-INSERT INTO `personal` VALUES (31,'anthoan','gonzalez','nose','3111','doctor'),(12345678,'diego','vergaline','nose@gmail.com','000000000','Doctor'),(12345679,'anthoan','gonzalez','aa','04145842747','Enfermera'),(31111553,'franchesco2','gonzalez','nose','3111','enfermero');
+INSERT INTO `personal` VALUES (31,'anthoan','gonzalez','nose','3111','doctor'),(31111553,'franchesco2','gonzalez','nose','3111','enfermero');
 /*!40000 ALTER TABLE `personal` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -300,7 +299,6 @@ CREATE TABLE `registro` (
   `fecha_r` date DEFAULT NULL,
   `cedula_h` int NOT NULL,
   `cod_examenes` int NOT NULL,
-  `observacion_examen` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
   KEY `cedula_h` (`cedula_h`),
   KEY `cod_examenes` (`cod_examenes`),
   CONSTRAINT `registro_ibfk_1` FOREIGN KEY (`cedula_h`) REFERENCES `historias` (`cedula_historia`),
@@ -326,4 +324,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-20 18:43:35
+-- Dump completed on 2024-09-02 11:22:55
