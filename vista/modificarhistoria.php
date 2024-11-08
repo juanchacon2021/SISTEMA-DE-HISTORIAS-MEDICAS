13c84b266c79b88d44fc1ce83058df5bf73bac32
Your Name <chaconjuan439@gmail.com>
2024-11-08 19:58:52 -0400

    Refactorizar estilos y scripts en el sistema de historias médicas

--- vista/modificarhistoria.php
+++ vista/modificarhistoria.php
@@ -122,7 +122,7 @@
                     </div>
                         
                     <div class="col-md-3">
-                        <label for="transsanguineo" class="texto-inicio font-medium">Trans Sanguineos</label>
+                        <label for="transsanguineo" class="texto-inicio font-medium">Transfuciones Sanguineas</label>
                         <input class="form-control bg-gray-200 rounded-lg border-white" type="text" id="transsanguineo"
                                 name="transsanguineo" value="<?php echo $paciente['transsanguineo']; ?>" required/>
                         <span id="stranssanguineo"></span>
