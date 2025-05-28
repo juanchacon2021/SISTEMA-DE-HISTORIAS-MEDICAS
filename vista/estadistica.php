757981e5d8be34237ae293b4849cdb35cc6d6a83
anthoan23 <anthoan.g23gmail.com>
2025-05-28 12:07:08 -0400

    Implement code changes to enhance functionality and improve performance

--- vista/estadistica.php
+++ vista/estadistica.php
@@ -9,7 +9,7 @@
         // Si no existe $permisos, redirigir a login
         header("Location: ?pagina=login");
         exit();
-    } elseif (!in_array('Estadisticas', $permisos)) {
+    } elseif (!in_array('Pacientes crónicos', $permisos)) {
         // Si existe $permisos pero no tiene acceso al módulo, mostrar error 403
         http_response_code(403);
         die('<div class="container text-center py-5">
